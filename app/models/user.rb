3d7566eb13775961456f37bd647f48fb5a930ea4
Paul Roman <romansp2@gmail.com>
2017-10-18 15:57:26 +0300

    User tests added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
 
+  validates :name, presence: true, length: {minimum: 4, maximum: 25}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+\.[a-z]+\z/i
+  validates :email, presence: true, length: {minimum: 10, maximum: 100},
+            format: {with: VALID_EMAIL_REGEX}
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(name: "Nikki", email: "O.Neil@gmail.com")
+  end
+  test 'shold be valid' do
+    assert @user.valid?
+  end
+ # test 'username should be not too long' do
+ #   @user.name = "m" * 29
+ #   assert_not @user.valid?
+ # end
+
+  test 'email should be not too long' do
+    @user.email = "a" * 100 + "@example.com"
+    assert_not @user.valid?
+  end
+
+  test 'correct email' do
+    valid_email = %w[uiomkk@gmail.com mion@ex.ua dk.sef@bk.ru]
+    valid_email.each do |valid|
+      @user.email = valid
+      assert @user.valid?, "#{valid.inspect} shold be valid"
+    end
+  end
+
 end
