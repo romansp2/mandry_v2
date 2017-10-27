7901ec4297daa73c19ae103b259bf47a937d6a65
Paul Roman <romansp2@gmail.com>
2017-10-27 03:51:28 +0300

    Main page рефакторинг 2

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+
+  def header_link_to_what
+    if request.original_url == root_url
+      "<a href=#search_page>ПОИСК</a>".html_safe
+    else
+      link_to 'ПОИСК', root_url + '#search_page'
+
+    end
+  end
 end
